60dc15da588853da3ec1ad308e14c65fd22458a5
Cristian <95005685+Criss117@users.noreply.github.com>
2025-03-16 12:46:04 -0500

    fix: ui issues fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,21 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 type ReplaceWithSpanOptions = {
-  htmlTag?: string;
-  className?: string;
+	htmlTag?: string;
+	className?: string;
 };
 
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
 export function replaceWithSpan(
-  text: string,
-  { htmlTag = "span", className = "" }: ReplaceWithSpanOptions = {}
+	text: string,
+	{ htmlTag = "span", className = "" }: ReplaceWithSpanOptions = {},
 ): string {
-  return text.replace(
-    /##(.*?)##/g,
-    `<${htmlTag} class="${className}">$1</${htmlTag}>`
-  );
+	return text.replace(
+		/##(.*?)##/g,
+		`<${htmlTag} class="${className}">$1</${htmlTag}>`,
+	);
 }
