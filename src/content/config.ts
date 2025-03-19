51a56290b89fb9837f015249f681da3aac6899cb
Cristian <95005685+Criss117@users.noreply.github.com>
2025-03-19 16:14:11 -0500

    refactor: worktogether

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,23 +2,35 @@ import { file } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const projects = defineCollection({
-  loader: file("src/content/projects.json"),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    description: z.string(),
-    demo: z.string().optional(),
-    short_description: z.string(),
-    images: z.object({
-      principal: z.string(),
-      rest: z.array(z.string()).max(4),
-    }),
-    tech: z.array(z.string()),
-    repository: z.string(),
-    real_site: z.string().optional(),
-  }),
+	loader: file("src/content/projects.json"),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		description: z.string(),
+		demo: z.string().optional(),
+		short_description: z.string(),
+		images: z.object({
+			principal: z.string(),
+			rest: z.array(z.string()).max(4),
+		}),
+		tech: z.array(z.string()),
+		repository: z.string(),
+		real_site: z.string().optional(),
+	}),
+});
+
+const about = defineCollection({
+	loader: file("src/content/about.json"),
+	schema: z.object({
+		icon: z.enum(["Book", "Medal", "Network", "AppWindow"]),
+		slug: z.string(),
+		title: z.string(),
+		description: z.string(),
+		className: z.string(),
+	}),
 });
 
 export const collections = {
-  projects,
+	projects,
+	about,
 };
